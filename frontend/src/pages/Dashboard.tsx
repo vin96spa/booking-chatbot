3201310b3ba4c7a72cd174216e361d9858c51130
Vincenzo Spagnolo <vinspa96@gmail.com>
2025-09-15 12:59:52 +0200

    fix scrolling on dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -13,7 +13,7 @@ function Dashboard() {
         <>
             <div className="fixed inset-0 bg-[linear-gradient(to_bottom,#62405A,#7B3F6D,#AC3557,#4E74A8,#9B658E)] bg-cover bg-no-repeat bg-center" />
 
-            <div className="relative min-h-screen w-full flex flex-col items-center justify-center">
+            <div className="fixed inset-0 overflow-hidden flex flex-col items-center justify-center">
                 <div className='flex gap-12 justify-center items-center'>
                     <div className='flex flex-col items-center'>
                         <h1 className='text-[#C2FEBD] text-3xl md:text-4xl font-bold mb-4 text-center'>EasyReserve</h1>
